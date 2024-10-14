745ab69a06c868f05eeb29dc86a7234d4974eb92
sairam0424 <uggesairam0000@gmail.com>
2024-10-14 12:47:28 +0530

    feat(POTD ) :  added the solution for the potd added the solution for the leetcode potd 14_10_24 in python

---
+++ POTD_14_10_24/Solution.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+'''
+
+#Linked list class
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        '''
+class Solution:
+    # Function to count nodes of a linked list.
+    def getCount(self, head):
+        # code here
+        
+        count = 0
+        
+        while head:
+            
+            count+=1
+            
+            head = head.next
+            
+        return count
